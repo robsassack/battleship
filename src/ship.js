ab306469b819bcf07fc6a57edf96af00fa9254ef
Rob Sassack <rsassack25@gmail.com>
2022-09-30 17:37:06 -0400

    Added Ship factory function

---
+++ src/ship.js
@@ -0,0 +1,14 @@
+const Ship = (length) => {
+  const hits = Array(length).fill(false);
+  const hit = (num) => {
+    hits[num] = true;
+  };
+  const isSunk = () => {
+    if (hits.includes(false)) {
+      return false;
+    }
+    return true;
+  };
+  return { length, hit, isSunk };
+};
+export default Ship;
