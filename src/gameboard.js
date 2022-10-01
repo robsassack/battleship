156856f740f62986c9432cd66d7844b1b5f970ef
Rob Sassack <rsassack25@gmail.com>
2022-10-01 00:03:14 -0400

    Added Gameboard factory function

---
+++ src/gameboard.js
@@ -0,0 +1,8 @@
+const Gameboard = () => {
+  const board = Array(10).fill().map(() => Array(10).fill(0));
+  const receiveAttack = (x, y) => {
+    board[x][y] = 1;
+  };
+  return { board, receiveAttack };
+};
+export default Gameboard;
