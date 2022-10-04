fab0f9d7e6c28266e8f478414572a61731a5de3a
Rob Sassack <rsassack25@gmail.com>
2022-10-03 20:58:25 -0400

    Added function to display game boards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const ship = require('./ship');
+const player = require('./player');
+
+const playerBoard = document.querySelector('#playerBoard');
+const computerBoard = document.querySelector('#computerBoard');
+
+const game = () => {
+  const human = player();
+  const computer = player();
+
+  human.playerBoard.placeShip(ship(4), 0, 0, 'horizontal');
+  computer.playerBoard.placeShip(ship(4), 0, 0, 'vertical');
+
+  const renderBoard = (board, container) => {
+    board.forEach((row, x) => {
+      const rowDiv = document.createElement('div');
+      rowDiv.classList.add('row');
+      row.forEach((cell, y) => {
+        const cellDiv = document.createElement('div');
+        cellDiv.classList.add('cell');
+        rowDiv.appendChild(cellDiv);
+        if (board[x][y] !== 0 && human.playerBoard.board[x][y] !== 1) {
+          cellDiv.classList.add('ship');
+        }
+      });
+      container.appendChild(rowDiv);
+    });
+  };
+
+  renderBoard(human.playerBoard.board, playerBoard);
+  renderBoard(computer.playerBoard.board, computerBoard);
+};
+
+game();
